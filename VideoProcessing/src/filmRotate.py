33e6b52f57fe58731816d0d7907e2cc3053ebf16
Sukuna963 <marquesleonardo284@gmail.com>
2024-01-02 19:08:23 -0300

    *

--- VideoProcessing/src/filmRotate.py
+++
@@ -1,8 +0,0 @@
-#import evything needed to edit video clips
-from moviepy.editor import *
-
-#load video and edit it
-video = VideoFileClip('./data/testimage.mp4').rotate(90)
-
-#write video
-video.write_videofile("./data/rotatevideo.mp4")
--- VideoProcessing/src/imageToMovie.py
+++
@@ -1,8 +0,0 @@
-#import everything needed to edit video clips
-from moviepy.editor import *
-
-#create a image sequence
-clip = ImageSequenceClip(['./data/1.jpg', './data/2.jpg'], fps = 1)
-
-#save output
-clip.write_videofile("./data/testimage.mp4")
